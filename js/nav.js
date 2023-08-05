d9162fc3c71fa288d0073b4031acea953a3df891
gocornejoc <gocornejoc@gmail.com>
2023-08-05 19:01:22 -0400

    error galeria solucionado, sin removeActive.

--- js/nav.js
+++ js/nav.js
@@ -10,25 +10,6 @@ $(".hover").mouseleave(
 
 
 
-  const images = document.querySelectorAll('.gal img');
-
-  // Función para agregar la clase 'active' al tocar la imagen
-  function addActiveClass() {
-    this.classList.add('active');
-  }
-
-  // Función para quitar la clase 'active' después de un tiempo determinado (2 segundos)
-  function removeActiveClass() {
-    setTimeout(() => {
-      this.classList.remove('active');
-    }, 2000);
-  }
-
-  // Agregar un EventListener a cada imagen para el evento 'touchstart'
-  images.forEach(img => {
-    img.addEventListener('touchstart', addActiveClass);
-    img.addEventListener('touchend', removeActiveClass);
-  });
-
+  
 
 
